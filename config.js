dad1af80167b7638782a7907a3f21937b4ac8266
mohamed223344553 <67484763+mohamed223344553@users.noreply.github.com>
2020-09-08 14:06:24 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NzUyOTk0MTE3MDI5NDYyMDU2.X1fuaw.fk6M-8rzjZyVbc9iW7n2KUaX39Y',    
+    TOKEN: 'NzUyOTk0MTE3MDI5NDYyMDU2.X1fuaw.3ZWAEgB4arYMVhaNScvss2JVA4k',    
     YT_API_KEY: 'api ', 
     prefix: '90',
     devs: ['427918590843551745']
